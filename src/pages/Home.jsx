fe1a6f95e4f86a3a25a91fbe550b8b16e395a1a2
JSP10877 <u20231212983@usco.edu.co>
2026-04-12 17:55:29 -0500

    Ajustes menores al Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import Navbar from '../components/Navbar'; // Importas la pieza de Lego
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div className="bg-slate-900 min-h-screen text-white">
+
+      {/* Menú pagina desde components/Navbar */}
       <Navbar paginaActiva="inicio"/>
       
       <main>
         {/* Aquí sigue el resto de tu contenido del home */}
         <h2 className="text-center text-4xl mt-10">Bienvenido a la cancha</h2>
       </main>
+      
     </div>
   );
 };
